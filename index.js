eb1d9d168f513544764312681b30c36a6928bb69
gweryf <priyam2007sp@gmail.com>
2022-10-16 10:57:53 +0530

    adding basic functionality to buttons

--- index.js
+++ index.js
@@ -1,4 +1,84 @@
-const one = document.querySelector(".one");
-one.addEventListener("click",function (e){
-    console.log(e);
+// selecting number buttons
+const one = document.querySelector('.one');
+const two = document.querySelector('.two');
+const three = document.querySelector('.three');
+const four = document.querySelector('.four');
+const five = document.querySelector('.five');
+const six = document.querySelector('.six');
+const seven = document.querySelector('.seven');
+const eight = document.querySelector('.eight');
+const nine = document.querySelector('.nine');
+const zero = document.querySelector('.zero');
+const period = document.querySelector('.period')
+
+// selecting operation buttons
+const plus = document.querySelector('.plus');
+const minus = document.querySelector('.minus');
+const mul = document.querySelector('.mul');
+const divide = document.querySelector('.divide');
+
+// adding basic functionality to all buttons
+
+one.addEventListener('click',() => {
+    console.log('clicked one');
+    document.querySelector('.idk').innerText += 1;
 });
+two.addEventListener('click',() => {
+    console.log('clicked two');
+    document.querySelector('.idk').innerText += 2;
+});
+three.addEventListener('click',() => {
+    console.log('clicked three');
+    document.querySelector('.idk').innerText += 3;
+});
+four.addEventListener('click',() => {
+    console.log('clicked four');
+    document.querySelector('.idk').innerText += 4;
+});
+five.addEventListener('click',() => {
+    console.log('clicked five');
+    document.querySelector('.idk').innerText += 5;
+});
+six.addEventListener('click',() => {
+    console.log('clicked six');
+    document.querySelector('.idk').innerText += 6;
+});
+seven.addEventListener('click',() => {
+    console.log('clicked seven');
+    document.querySelector('.idk').innerText += 7;
+});
+eight.addEventListener('click',() => {
+    console.log('clicked eight');
+    document.querySelector('.idk').innerText += 8;
+});
+nine.addEventListener('click',() => {
+    console.log('clicked nine');
+    document.querySelector('.idk').innerText += 9;
+});
+zero.addEventListener('click', ()=>{
+    console.log('clicked zero');
+    document.querySelector('.idk').innerText += 0;
+})
+period.addEventListener('click', ()=>{
+    console.log('clicked period');
+    document.querySelector('.idk').innerText += '.';
+})
+
+
+
+plus.addEventListener('click', ()=>{
+    console.log('clicked plus');
+    document.querySelector('.idk').innerText += '+';
+})
+minus.addEventListener('click', ()=>{
+    console.log('clicked minus');
+    document.querySelector('.idk').innerText += '-';
+})
+mul.addEventListener('click', ()=>{
+    console.log('clicked mul');
+    document.querySelector('.idk').innerText += '*';
+})
+divide.addEventListener('click', ()=>{
+    console.log('clicked divide');
+    document.querySelector('.idk').innerText += '/';
+})
