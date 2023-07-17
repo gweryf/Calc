9b8d82df80bf456e2bd7f11c9495e975af4ba7a1
Priyam Shrivastava <priyam2007sp@gmail.com>
2023-07-17 22:52:07 +0530

    fixed the display styling of output

--- index.js
+++ index.js
@@ -1,76 +1,84 @@
 // selecting various buttons for the calculator
-const numberButton = document.querySelectorAll('[data-number]')
-const operationButtons = document.querySelectorAll('[data-operation]')
-const equalsButton = document.querySelector('[data-equals]')
-const delButton = document.querySelector('[data-delete]')
-const alcButton = document.querySelector('[data-all-clear]')
-const previousoperand = document.querySelector('[data-previous-operand]')
-const currentoperand = document.querySelector('[data-current-operand]')
+const numberButton = document.querySelectorAll("[data-number]");
+const operationButtons = document.querySelectorAll("[data-operation]");
+const equalsButton = document.querySelector("[data-equals]");
+const delButton = document.querySelector("[data-delete]");
+const alcButton = document.querySelector("[data-all-clear]");
+const previousoperand = document.querySelector("[data-previous-operand]");
+const currentoperand = document.querySelector("[data-current-operand]");
 
-
-let operator = '+'
+let operator = "+";
 // creating function to evaluate the numbers
-function evaluate (previousoperand, currentoperand) {
-    console.log(previousoperand);
-    console.log(currentoperand);
-    let result = 0
-    let previous = parseFloat(previousoperand)
-    let  current = parseFloat(currentoperand)
-    switch(operator) {
-        case '+':
-            result = previous+current
-            break
-        case '-':
-            result = previous-current
-            break
-        case '*':
-            result = previous*current
-            break
-        case '÷':
-            result = current/previous
-            break
-        default:
-            return
-    }
-    return result
+function evaluate(previousoperand, currentoperand) {
+  console.log(previousoperand);
+  console.log(currentoperand);
+  let result = 0;
+  let previous = parseFloat(previousoperand);
+  let current = parseFloat(currentoperand);
+  switch (operator) {
+    case "+":
+      result = previous + current;
+      break;
+    case "-":
+      result = previous - current;
+      break;
+    case "*":
+      result = previous * current;
+      break;
+    case "÷":
+      result = current / previous;
+      break;
+    default:
+      return;
+  }
+  return result;
 }
 
-numberButton.forEach(button=>{
-    button.addEventListener('click', (e)=>{
-        console.log(e.target);
-        currentoperand.innerText += e.target.innerText 
-    })
-})
+numberButton.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    console.log(e.target);
+    currentoperand.innerText += e.target.innerText;
+  });
+});
 
-operationButtons.forEach(button=>{
-    button.addEventListener('click', (e)=>{
-        if (currentoperand.innerText === '') return
-        if (previousoperand.innerText !== '') {
-            console.log(evaluate(currentoperand.innerText, previousoperand.innerText));
-            previousoperand.innerText = evaluate(currentoperand.innerText, previousoperand.innerText)
-            currentoperand.innerText = ''
-        } else {
-            previousoperand.innerText = currentoperand.innerText 
-        }
-        console.log(e.target);
-        operator = e.target.innerText
-        currentoperand.innerText += e.target.innerText 
-        // previousoperand.innerText = currentoperand.innerText
-        currentoperand.innerText = ''
-    })
-})
+operationButtons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    if (currentoperand.innerText === "") return;
+    if (previousoperand.innerText !== "") {
+      console.log(
+        evaluate(currentoperand.innerText, previousoperand.innerText)
+      );
+      previousoperand.innerText = evaluate(
+        currentoperand.innerText,
+        previousoperand.innerText
+      );
+      currentoperand.innerText = "";
+    } else {
+      previousoperand.innerText = currentoperand.innerText + button.innerText;
+    }
+    console.log(e.target);
+    operator = e.target.innerText;
+    currentoperand.innerText += e.target.innerText;
+    // previousoperand.innerText = currentoperand.innerText
+    currentoperand.innerText = "";
+  });
+});
 
-alcButton.addEventListener('click',()=>{
-    currentoperand.innerText = ''
-    previousoperand.innerText = ''
-})
+alcButton.addEventListener("click", () => {
+  currentoperand.innerText = "";
+  previousoperand.innerText = "";
+});
 
-equalsButton.addEventListener('click',()=>{
-    if(previousoperand.innerText === '' || currentoperand.innerText === '') return
-    currentoperand.innerText = evaluate(currentoperand.innerText, previousoperand.innerText)
-    previousoperand.innerText = ''
-})
+equalsButton.addEventListener("click", () => {
+  if (previousoperand.innerText === "" || currentoperand.innerText === "")
+    return;
+  currentoperand.innerText = evaluate(
+    currentoperand.innerText,
+    previousoperand.innerText
+  );
+  previousoperand.innerText = "";
+});
 
-delButton.addEventListener('click',()=>{
-    currentoperand.innerText = currentoperand.innerText.slice(0,-1)
-})
+delButton.addEventListener("click", () => {
+  currentoperand.innerText = currentoperand.innerText.slice(0, -1);
+});
